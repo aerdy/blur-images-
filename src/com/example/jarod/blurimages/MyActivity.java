f7d3addc93f6c3626aa11ee65421c89fb86ae75f
aerdy <aerdy4@gmail.com>
2014-09-10 12:21:04 +0700

    project

---
+++ src/com/example/jarod/blurimages/MyActivity.java
@@ -0,0 +1,102 @@
+package com.example.jarod.blurimages;
+
+import com.example.blurimages.R;
+
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
+import android.os.Bundle;
+import android.renderscript.Allocation;
+import android.renderscript.RenderScript;
+import android.renderscript.ScriptIntrinsicBlur;
+import android.view.View;
+import android.view.ViewTreeObserver;
+import android.view.ViewTreeObserver.OnGlobalLayoutListener;
+import android.view.ViewTreeObserver.OnPreDrawListener;
+import android.view.WindowManager;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class MyActivity extends Activity {
+    private ImageView mImage;
+    private TextView mText;
+
+    private OnPreDrawListener mPreDrawListener = new OnPreDrawListener() {
+        @Override
+        public boolean onPreDraw() {
+            ViewTreeObserver observer = mText.getViewTreeObserver();
+            if(observer != null) {
+                observer.removeOnPreDrawListener(this);
+            }
+            Drawable drawable = mImage.getDrawable();
+            if (drawable != null && drawable instanceof BitmapDrawable) {
+                Bitmap bitmap = ((BitmapDrawable) drawable).getBitmap();
+                if (bitmap != null) {
+                    blur(bitmap, mText, 25);
+                }
+            }
+            return true;
+        }
+    };
+
+    private OnGlobalLayoutListener mLayoutListener = new OnGlobalLayoutListener() {
+
+        @Override
+        public void onGlobalLayout() {
+            ViewTreeObserver observer = mText.getViewTreeObserver();
+            if(observer != null) {
+                observer.addOnPreDrawListener(mPreDrawListener);
+            }
+        }
+    };
+
+    /**
+     * Called when the activity is first created.
+     */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_BLUR_BEHIND, WindowManager.LayoutParams.FLAG_BLUR_BEHIND);
+        setContentView(R.layout.activity_my);
+        mImage = (ImageView) findViewById(R.id.image);
+        mText = (TextView)findViewById(R.id.text);
+        if (mImage != null && mText != null) {
+            ViewTreeObserver observer = mText.getViewTreeObserver();
+            if (observer != null) {
+                observer.addOnGlobalLayoutListener(
+                        mLayoutListener);
+            }
+        }
+    }
+
+    @SuppressLint("NewApi")
+	private void blur(Bitmap bkg, View view, float radius) {
+
+        Bitmap overlay = Bitmap.createBitmap(view.getMeasuredWidth(), view.getMeasuredHeight(), Bitmap.Config.ARGB_8888);
+
+        Canvas canvas = new Canvas(overlay);
+        canvas.drawBitmap(bkg, -view.getLeft(), -view.getTop(), null);
+
+        RenderScript rs = RenderScript.create(this);
+
+        Allocation overlayAlloc = Allocation.createFromBitmap(rs, overlay);
+
+        ScriptIntrinsicBlur blur = ScriptIntrinsicBlur.create(rs, overlayAlloc.getElement());
+
+        blur.setInput(overlayAlloc);
+
+        blur.setRadius(radius);
+
+        blur.forEach(overlayAlloc);
+
+        overlayAlloc.copyTo(overlay);
+
+        view.setBackground(new BitmapDrawable(getResources(), overlay));
+
+        rs.destroy();
+    }
+
+}
